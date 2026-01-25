494082e186fc182d22611a028cc515ace6ff8a84
Yara-Silva05 <yara.silva.alves05@gmail.com>
2026-01-25 15:48:37 -0300

    Feat: implementação do endpoint saveProduct

--- src/main/java/com/study/firstProject/controllers/ProductController.java
+++ src/main/java/com/study/firstProject/controllers/ProductController.java
@@ -1,14 +1,30 @@
 package com.study.firstProject.controllers;
 
-import com.study.firstProject.repositories.ProductRepositories;
+import com.study.firstProject.dtos.ProductRecordDto;
+import com.study.firstProject.models.ProductModel;
+import com.study.firstProject.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
     @Autowired
-    ProductRepositories productRepositories;
+    ProductRepository productRepository;
 
-
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
+
+
+
--- src/main/java/com/study/firstProject/dtos/ProductRecordDto.java
+++ src/main/java/com/study/firstProject/dtos/ProductRecordDto.java
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal vaLlue) {
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
 }
